c6f40af320f571db1eb82bbd821a3bf52009bde8
Ruanpabloescobar <celyannelima634@gmail.com>
2024-11-18 20:50:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual a camisa do ronaldinho no milan?',
+    'A camisa 80'
+)
+
+criaCartao(
+    'MMA',
+    'quem foi o vencedor na luta de Mike Tyson e Jake Paul?',
+    'Jake Paul ;-;'
+)
+
+criaCartao(
+    'Futebol',
+    'Messi ou CR7?',
+    'pele'
+)
+
+criaCartao(
+    'porno',
+    'Mia Khalifa ou Lana Rhoades ?',
+    'Lana Rhoades '
+)
